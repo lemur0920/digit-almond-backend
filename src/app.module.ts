12758b303ef8f780225272c1b25763e7f31d9c89
lemur0920 <lemur12@naver.com>
2025-07-30 21:02:32 +0900

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TodosModule } from './todos/todos.module';
+import { PostsModule } from './posts/posts.module';
+import { MissionsModule } from './missions/missions.module';
+import { BadgesModule } from './badges/badges.module';
+import { CitiesModule } from './cities/cities.module';
+import { AlarmsModule } from './alarms/alarms.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, TodosModule, PostsModule, MissionsModule, BadgesModule, CitiesModule, AlarmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
