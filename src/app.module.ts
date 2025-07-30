7d813c2c6aea63e5dfb2275e878ca305f4d3f72b
lemur0920 <lemur12@naver.com>
2025-07-30 22:10:10 +0900

    prisma 모듈, 서비스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,11 @@ import { MissionsModule } from './missions/missions.module';
 import { BadgesModule } from './badges/badges.module';
 import { CitiesModule } from './cities/cities.module';
 import { AlarmsModule } from './alarms/alarms.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, TodosModule, PostsModule, MissionsModule, BadgesModule, CitiesModule, AlarmsModule],
+  imports: [UsersModule, AuthModule, TodosModule, PostsModule, MissionsModule, BadgesModule, CitiesModule, AlarmsModule, PrismaModule, ConfigModule.forRoot(({ isGlobal: true }))],
   controllers: [AppController],
   providers: [AppService],
 })
